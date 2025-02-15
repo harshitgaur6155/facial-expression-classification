f1e1e6e56b119074a1ca635fab26faaf5f8fba47
Harshit Gaur <harshitgaur@Harshits-Air.attlocal.net>
2025-02-15 00:25:23 -0500

    Model Training Pipeline: Setup & Completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from facialExpressionClassify import logger
 from facialExpressionClassify.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from facialExpressionClassify.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from facialExpressionClassify.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 
 
@@ -21,6 +22,19 @@ STAGE_NAME = "Base Model Preparation Stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    obj = PrepareBaseModelTrainingPipeline()
+   obj.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logger.exception(e)
+   raise e
+
+
+
+
+STAGE_NAME = "Model Training Stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   obj = ModelTrainingPipeline()
    obj.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
