b722320bebb1fd9bd70204a89a41a3ae3d83a5a5
Harshit Gaur <harshit.gaur6155@gmail.com>
2025-02-17 17:17:16 -0500

    Project Template Python FIle: Revised

--- template.py
+++ template.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 import logging
+import yaml
+
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
@@ -18,6 +20,10 @@ list_of_files = [
     f'src/{project_name}/entity/__init__.py',
     f'src/{project_name}/constants/__init__.py',
     f'src/{project_name}/exception/__init__.py',
+    f'templates/index.html',
+    f'static/.placeholder',
+    'app.py',
+    'main.py',
     'config/config.yaml',
     'dvc.yaml',
     'params.yaml',
@@ -26,6 +32,12 @@ list_of_files = [
     'research/trials.ipynb'
 ]
 
+
+
+# Default YAML content to add to yaml files
+yaml_content = {
+    "key1": "val1"
+}
 
 for filepath in list_of_files:
     filepath = Path(filepath)
@@ -38,6 +50,10 @@ for filepath in list_of_files:
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as wr:
             logging.info(f"Creating empty file: {filepath}")
-        
+            
+            # If the file is yaml, write YAML content into it
+            if filepath in (Path('config/config.yaml'), Path('params.yaml'), Path('dvc.yaml')):
+                yaml.dump(yaml_content, wr, default_flow_style=False)
+                logging.info(f"Added YAML content to {filepath}")
     else:
         logging.info(f"{filename} already exists")
